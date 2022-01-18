67d8d7f0689733f9051f4c7722fb137ab006fc2c
Santiago-delRio <santiago.delrio313@gmail.com>
2022-01-18 00:27:18 -0300

    probando algo

--- pages/area-pastoral/caritas/index.js
+++ pages/area-pastoral/caritas/index.js
@@ -1,18 +1,39 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect } from 'react'
 import headerImg from "../../../assets/imagenes/sacramentos/comunion/header.jpg"
 import caritasStyles from "./caritas.module.scss"
 
-const Caritas = () => {
+const Caritas = ({ nombre }) => {
+
+    useEffect(()=>{
+        console.log(nombre)
+    },[])
+
     return (
         <main className={caritasStyles.caritas}>
             <Head>
                 <title>Cáritas - Parroquia Santa Juana de Arco</title>
             </Head>
             <h1>Caritas</h1>
+            <h2>{nombre.nombre}</h2>
+            
         </main>
     );
 }
  
 export default Caritas;
+
+
+export async function getStaticProps(){
+
+    const res = await fetch(`${process.env.SERVER_IP}/hola`)
+
+    const nombre = await res.json()
+  
+    return{
+        props: { nombre },
+        revalidate: 120,
+    }
+}
