93f58b424c675f4299e13a92744a9f9fcf075f99
Santiago-delRio <santiago.delrio313@gmail.com>
2022-03-11 20:57:28 -0300

    ahora el formulario de contacto usa sendgrid en vez de nodemailer

--- pages/api/contacto.js
+++ pages/api/contacto.js
@@ -1,54 +1,28 @@
+import sendgrid from "@sendgrid/mail";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
 export default async (req, res) => {
 
-    const nodemailer = require('nodemailer')
-
-    //Mensaje del mail
-    const mensaje = `
+  // Mensaje del mail
+  const mensaje = `
     Email: ${req.body.email}\n
     Nombre: ${req.body.nombre}\n
     Telefono: ${req.body.telefono}\n
     ……………\n
     ${req.body.mensaje}
-    `
+    `;
 
-    //Armar el mail
-    const datosMail = {
-        from: process.env.MAIL_CONTACTO,
-        to: process.env.CONTACTO_MAIL_RECEPCION,
-        subject: `Mensaje de ${req.body.email} - ${req.body.nombre}`,
-        text: mensaje,
-    }
-
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-            user: process.env.MAIL_CONTACTO,
-            pass: process.env.MAIL_PASSWORD,
-        },
-        secure: true,
-    })
-
-    await new Promise((resolve, reject) => {
-        
-        transporter.verify(function (error, success) {
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                console.log("Server is ready to take our messages");
-                resolve(success);
-            }
-        });
+  try {
+    await sendgrid.send({
+      to: process.env.CONTACTO_MAIL_RECEPCION,
+      from: process.env.MAIL_CONTACTO,
+      subject: `Mensaje de ${req.body.email} - ${req.body.nombre}`,
+      text: mensaje,
     });
+  } catch (error) {
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
 
-    await new Promise((resolve, reject) => {
-        //Enviar mail
-        transporter.sendMail(datosMail, function (err, info) {
-            if(err)
-                reject(err)
-            else
-                res.send("Exitoso")
-        })
-    });
-}
+  return res.status(200).json({ error: "" });
+};
